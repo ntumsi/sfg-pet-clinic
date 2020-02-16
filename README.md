fbe04aa9d322c79d0f966b787c9ff767f1b99fc6
ntumsi <lah_ntumsi2000@yahoo.com>
2020-02-16 08:58:47 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# SFG-pet-clinic" 
---
+++ src/test/java/com/springframework/SFGpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springframework.SFGpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
